e6a39f7ac5ce65200ff27838419af4d28dfecb9c
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-14 23:27:00 +0500

    task1 done

---
+++ task01.py
@@ -0,0 +1,7 @@
+first_name_age = "Salom, mening ismim {} va yoshim {}"
+
+first_name = input("Ismingizni kiriting: ").capitalize()
+
+age = input("Yoshingizni kiriting: ")
+
+print(first_name_age.format(first_name, age, ))
